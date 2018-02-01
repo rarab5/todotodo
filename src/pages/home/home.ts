f41184a17dc2a06d7fc70e0b04060735f53183f9
rarab5 <33835690+rarab5@users.noreply.github.com>
2018-02-01 11:44:51 +0100

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item';
+import { ItemDetailPage } from '../item-detail/item-detail';
+import { Data } from '../../providers/data/data';
+
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  public items = [];
+ 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: Data) {
+ 
+    this.dataService.getData().then((todos) => {
+ 
+      if(todos){
+        this.items = todos;
+      }
+ 
+    });
+ 
+  }
+ 
+  addItem(){
+ 
+    let addModal = this.modalCtrl.create(AddItemPage);
+ 
+    addModal.onDidDismiss((item) => {
+ 
+          if(item){
+            this.saveItem(item);
+          }
+ 
+    });
+ 
+    addModal.present();
+ 
+  }
+ 
+  saveItem(item){
+    this.items.push(item);
+  }
+ 
+  viewItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+}
